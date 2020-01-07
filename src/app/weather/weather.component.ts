f3be6c6ff17fd80f104d6f5baa77778244cb0cf2
unknown <lblondeau.megasoft@gmail.com>
2020-01-07 20:39:21 +0100

    fonctions de récupération de données

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { weather_data } from './weather_data';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { current_data } from './current_data';
 
 
 
@@ -13,12 +14,17 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 export class WeatherComponent implements OnInit {
 
    donnee = new weather_data();
+
     public latitude :number = 35;
     public longitude : number = 100;
     public units : string ='metric';
-    public forecast_data : weather_data[] = []; 
     public  API_KEY:string ="33d32c6a2760f1561d57c0f8229f0a6a";
     public URL: string ="http://api.openweathermap.org/data/2.5/";
+
+    public forecast_data : weather_data[] = []; 
+    public hourly_data : current_data[]=[];
+    public weekly_data : current_data[]= [];
+   
     param_weather = new HttpParams()
           .set('lat',this.latitude.toString())
           .set('lon', this.longitude.toString())
@@ -90,6 +96,93 @@ export class WeatherComponent implements OnInit {
     get_image_icon(id_icon){
       return "http://openweathermap.org/img/wn/"+id_icon+"@2x.png"
     }
+    updater(){
+      setInterval(function(){
+        this.getData("weather");
+      },600000);
+    }
+    fill_data(){
+      
+      for(var i=0;i<0; i++){
+        
+
+          let hourly_data_temp = new current_data();
+
+          if(i==0){
+            hourly_data_temp.name = "Aujourd'hui"+this.get_heure(this.forecast_data[i].dt) ;
+          }
+          else if((i!=0)&&(this.get_namedate(this.forecast_data[i-1].dt)!=this.get_namedate(this.forecast_data[i].dt))){
+            hourly_data_temp.name = "Demain"+this.get_heure(this.forecast_data[i].dt) ;
+          }else{
+            hourly_data_temp.name = this.get_heure(this.forecast_data[i].dt) ;
+          }
+
+          
+          hourly_data_temp.img_url=this.get_image_icon(this.forecast_data[i].id_icon) ;
+          hourly_data_temp.temperature.push(this.forecast_data[i].temperature);
+  
+          hourly_data_temp.humidity.push(this.forecast_data[i].humidity);
+  
+          //current_data_temp.rainfall=this.forecast_data[i].;
+          
+          this.hourly_data.push(hourly_data_temp);
+
+          
+        }
+      let day: number[] = [];
+      let val: number = 0;
+
+      for (var i=1; i < this.forecast_data.length; i++ ){
+        if(this.get_namedate(this.forecast_data[i-1].dt)==this.get_namedate(this.forecast_data[i].dt)){
+          val++;
+        }
+        else{
+          day.push(val+1);
+          val=0;
+        }
+        
+      }
+      let t: number = 0;
+
+      for(var i=0; i<day.length; i++){
+        let weekly_data_temp: current_data;
+        let temp_min: number= this.forecast_data[i].temperature;
+        let temp_max: number= this.forecast_data[i].temperature;
+        let humidity_min: number = this.forecast_data[i].humidity;
+        let humidity_max: number= this.forecast_data[i].humidity;
+        let id_icon_temp: string;
+        
+        weekly_data_temp.name= this.get_namedate(this.forecast_data[i].dt);
+
+        for(var j= t; j<t+day[i];j++){
+          if(this.forecast_data[j].temperature >temp_max){
+            temp_max=this.forecast_data[j].temperature;
+            id_icon_temp=this.forecast_data[j].id_icon;
+          }
+          if(this.forecast_data[j].temperature <temp_min){
+            temp_max=this.forecast_data[j].temperature;
+          }
+          if(this.forecast_data[j].humidity >humidity_max){
+            humidity_max=this.forecast_data[j].humidity;
+          }
+          if(this.forecast_data[j].humidity <humidity_min){
+            humidity_max=this.forecast_data[j].humidity;
+          }
+          t++;
+        }
+
+        weekly_data_temp.humidity.push(humidity_min);
+        weekly_data_temp.humidity.push(humidity_max);
+        weekly_data_temp.temperature.push(temp_min);
+        weekly_data_temp.temperature.push(temp_max);
+        weekly_data_temp.img_url= this.get_image_icon(id_icon_temp);
+        this.weekly_data.push(weekly_data_temp);
+      }
+        
+
+    }
+      
+    
 
   ngOnInit() {
     //this.getData("weather");
